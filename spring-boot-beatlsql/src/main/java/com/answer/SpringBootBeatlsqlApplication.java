5a572dade1a3f48adaf8e7305c7c85afed0ad5bc
suchao <persistsuchao@hotmail.com>
2019-01-30 18:24:07 +0800

    init commit

---
+++ spring-boot-beatlsql/src/main/java/com/answer/SpringBootBeatlsqlApplication.java
@@ -0,0 +1,14 @@
+package com.answer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBeatlsqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBeatlsqlApplication.class, args);
+	}
+
+}
+
