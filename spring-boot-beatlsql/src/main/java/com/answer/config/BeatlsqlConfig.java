bef82ed7d062d4c5bf6f3b6f14f1fe5ddd293ada
suchao <persistsuchao@hotmail.com>
2019-01-31 17:39:32 +0800

    整合  beatsql

---
+++ spring-boot-beatlsql/src/main/java/com/answer/config/BeatlsqlConfig.java
@@ -0,0 +1,105 @@
+package com.answer.config;
+
+import org.beetl.core.resource.WebAppResourceLoader;
+import org.beetl.ext.spring.BeetlGroupUtilConfiguration;
+import org.beetl.ext.spring.BeetlSpringViewResolver;
+import org.beetl.sql.core.ClasspathLoader;
+import org.beetl.sql.core.Interceptor;
+import org.beetl.sql.core.UnderlinedNameConversion;
+import org.beetl.sql.core.db.MySqlStyle;
+import org.beetl.sql.ext.DebugInterceptor;
+import org.beetl.sql.ext.spring4.BeetlSqlDataSource;
+import org.beetl.sql.ext.spring4.BeetlSqlScannerConfigurer;
+import org.beetl.sql.ext.spring4.SqlManagerFactoryBean;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.autoconfigure.transaction.PlatformTransactionManagerCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternUtils;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+
+/**
+ * @descreption
+ * @Author answer
+ * @Date 2019/1/31 16 27
+ */
+
+@Configuration
+public class BeatlsqlConfig {
+
+    @Bean(initMethod = "init", name = "beetlConfig")
+    public BeetlGroupUtilConfiguration getBeetlGroupUtilConfiguration() {
+        BeetlGroupUtilConfiguration beetlGroupUtilConfiguration = new BeetlGroupUtilConfiguration();
+        ResourcePatternResolver patternResolver = ResourcePatternUtils.getResourcePatternResolver(new DefaultResourceLoader());
+        try {
+            WebAppResourceLoader webAppResourceLoader =
+                    new WebAppResourceLoader(patternResolver.getResource("classpath:/templates").getFile().getPath());
+            beetlGroupUtilConfiguration.setResourceLoader(webAppResourceLoader);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //读取配置文件信息
+        return beetlGroupUtilConfiguration;
+    }
+
+
+    @Bean(name = "beetlViewResolver")
+    public BeetlSpringViewResolver getBeetlSpringViewResolver(@Qualifier("beetlConfig") BeetlGroupUtilConfiguration beetlGroupUtilConfiguration) {
+        BeetlSpringViewResolver beetlSpringViewResolver = new BeetlSpringViewResolver();
+        beetlSpringViewResolver.setContentType("text/html;charset=UTF-8");
+        beetlSpringViewResolver.setOrder(0);
+        beetlSpringViewResolver.setConfig(beetlGroupUtilConfiguration);
+        return beetlSpringViewResolver;
+    }
+
+    /**
+     * 功能描述：
+     * 配置包扫描
+     *
+     * @param
+     * @return
+     * @author chao.su
+     * @date 2019/1/31 16:49
+     */
+    @Bean(name = "beetlSqlScannerConfigurer")
+    public BeetlSqlScannerConfigurer getBeetlSqlScannerConfigurer() {
+        BeetlSqlScannerConfigurer configurer = new BeetlSqlScannerConfigurer();
+        configurer.setBasePackage("com.answer.dao");
+        configurer.setDaoSuffix("Dao");
+        configurer.setSqlManagerFactoryBeanName("sqlManagerFactoryBean");
+        return configurer;
+    }
+
+    @Bean(name = "sqlManagerFactoryBean")
+    @Primary
+    public SqlManagerFactoryBean getSeqlManagerFactoryBean(@Qualifier("datasource") DataSource datasource) {
+        SqlManagerFactoryBean factory = new SqlManagerFactoryBean();
+        BeetlSqlDataSource source = new BeetlSqlDataSource();
+        source.setMasterSource(datasource);
+        factory.setCs(source);
+        factory.setDbStyle(new MySqlStyle());
+        factory.setInterceptors(new Interceptor[]{new DebugInterceptor()});
+        //开启驼峰
+        factory.setNc(new UnderlinedNameConversion());
+        //sql file path
+        factory.setSqlLoader(new ClasspathLoader("/sql"));
+        return factory;
+    }
+
+    /**
+     * 配置数据库
+     * @return
+     */
+    @Bean(name = "datasource")
+    public DataSource getDataSource() {
+        return DataSourceBuilder.create().url("jdbc:mysql://127.0.0.1:3306/test").username("root").password("root").build();
+    }
+
+
+}
