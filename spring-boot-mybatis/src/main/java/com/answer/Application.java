a5ccad4206b93e91eb783d05eb6b93d701cccd2d
suchao <persistsuchao@hotmail.com>
2019-02-01 16:47:49 +0800

    整合  mybatis

--- spring-boot-mybatis/src/main/java/com/answer/Application.java
+++ spring-boot-mybatis/src/main/java/com/answer/Application.java
@@ -1,9 +1,17 @@
 package com.answer;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @descreption
  * @Author answer
  * @Date 2019/2/1 15 46
  */
+@SpringBootApplication
 public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
