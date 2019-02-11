992515cee05687dd422619d049487c0e49a1ebb3
suchao <persistsuchao@hotmail.com>
2019-02-11 15:30:27 +0800

    mybatis 多数据源

---
+++ spring-boot-mybatis-mulidatasource/src/main/java/com/answer/Application.java
@@ -0,0 +1,17 @@
+package com.answer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @descreption
+ * @Author answer
+ * @Date 2019/2/11 15 28
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
