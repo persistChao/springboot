befac5be4552c6e4b2598b516a2ef372c1d9e871
suchao <persistsuchao@hotmail.com>
2019-02-12 16:30:48 +0800

    spring boot cache

---
+++ spring-boot-cache/src/main/java/com/answer/CacheApplication.java
@@ -0,0 +1,17 @@
+package com.answer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @descreption
+ * @Author answer
+ * @Date 2019/2/12 10 05
+ */
+@SpringBootApplication
+public class CacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CacheApplication.class, args);
+    }
+}
---
+++ spring-boot-cache/src/main/java/com/answer/model/Book.java
@@ -0,0 +1,28 @@
+package com.answer.model;
+
+/**
+ * @descreption
+ * @Author answer
+ * @Date 2019/2/12 10 06
+ */
+public class Book {
+    private String isbn;
+
+    private String title;
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
