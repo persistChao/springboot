bcf884f6dfff5ae3e2659060ed7d4887c8d44f58
suchao <suchao01@zhuanche.com>
2019-06-05 16:45:15 +0800

    spring boot cache

---
+++ spring-boot-web/src/main/java/com/answer/service/HelloService.java
@@ -0,0 +1,14 @@
+package com.answer.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @descreption
+ * @Author answer
+ * @Date 2019/6/5 15 19
+ */
+@Service
+public class HelloService {
+
+
+}
--- spring-boot-web/src/main/java/com/answer/web/HelloController.java
+++ spring-boot-web/src/main/java/com/answer/web/HelloController.java
@@ -1,14 +1,52 @@
 package com.answer.web;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
 
 @RestController
 public class HelloController {
+
+    private DeferredResult<String> deferredResult = new DeferredResult<>();
+
+//    @Autowired
+//    private AsyncService asyncService;
 
     @RequestMapping("/hello")
     public String hello() {
         return "Hello spring boot!";
     }
+
+    @GetMapping("async")
+    public DeferredResult<Object> async() {
+        DeferredResult<Object> response = new DeferredResult<Object>(3000L, null);
+        response.onCompletion(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("处理程序");
+            }
+        });
+        response.setResult(new Object());
+        return response;
+    }
+
+    @GetMapping("void")
+    public void tr() {
+        System.out.println("s");
+    }
+
+    @RequestMapping("/testDeferredResult")
+    public DeferredResult<String> testDeferredResult() {
+        return deferredResult;
+    }
+
+    @RequestMapping("/setDeferredResult")
+    public String setDeferredResult() {
+        deferredResult.setResult("Test result!");
+        return "succeed";
+    }
+
 }
